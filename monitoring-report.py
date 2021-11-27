71ac023f85f28128e26075501ba777ba394e6e8e
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-11-27 12:48:46 +0100

    finish gateway impl

--- monitoring-report.py
+++ monitoring-report.py
@@ -12,10 +12,11 @@ import grp
 import requests
 
 ENCODING = "UTF-8"
+NO_GROUP = "nogroup"
 
 def dropPivileges(uid_name, gid_name=None):
     if not gid_name:
-        gid_name = "nogroup"
+        gid_name = NO_GROUP 
 
     target_uid = pwd.getpwnam(uid_name).pw_uid
     target_gid = grp.getgrnam(gid_name).gr_gid
@@ -26,9 +27,9 @@ def dropPivileges(uid_name, gid_name=None):
 def splitCMD(cmd):
     return list(filter(lambda a: a,cmd.strip("\n").split(" ")))
 
-def executeAndSubmit(hostnameIdent, user, args):
+def executeAndSubmit(hostnameIdent, user, service, cmd, args):
 
-    if not args.no_sudo:
+    if not args.ignore_user:
         dropPivileges(user)
 
     message = ""
@@ -37,7 +38,7 @@ def executeAndSubmit(hostnameIdent, user, args):
     # run monitoring command
     try:
         p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding=ENCODING)
-        message = "{}\t{}\t{}\t{}\n".format(hostnameIdent, serviceName, p.returncode, p.stdout)
+        message = "{}\t{}\t{}\t{}\n".format(hostnameIdent, service, p.returncode, p.stdout)
     except FileNotFoundError:
         print("{} command not found!".format(splitCMD(cmd)[0]), file=sys.stderr)
 
@@ -51,8 +52,10 @@ def executeAndSubmit(hostnameIdent, user, args):
             status = "CRITICAL"
             info   = p.stderr
 
-        r = requests.post(args.gateway, json={ "service" : args.hostname,
-                "token" : args.token, "status" : status, "info" : info }
+        print(args.gateway, { "service" : service,
+                "token" : args.gateway_token, "status" : status, "info" : info })
+        r = requests.post(args.gateway, json={ "service" : service,
+                "token" : args.gateway_token, "status" : status, "info" : info })
         r.raise_for_status()
 
     else:
@@ -61,40 +64,23 @@ def executeAndSubmit(hostnameIdent, user, args):
         else:
             nscaCMD = [args.nsca_bin]
 
-        nscaProcess = sp.Popen(nscaCMD, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        nscaProcess = subprocess.Popen(nscaCMD, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         stdout = nscaProcess.communicate(input=bytes(message, ENCODING))
         if nscaProcess.returncode != 0:
             raise RuntimeError("Execution of send_nsca failed - {}".format(stdout))
-
-
-def executeAndSubmitAsync(user, service, cmd, sudo):
-    p = Process(target=executeAndSubmit, args=(user, service, cmd, no_sudo,))
-    p.start()
-    return p
 
 def executeConfig(hostnameIdent, args):
 
     asyncTasks = []
 
     # parse config and start tasks
-    with open(args.filename, "r") as f:
+    with open(args.config, "r") as f:
 
         for line in f:
 
             # split config #
-            user, serviceName, cmd = list(filter(lambda x: x, line.split("\t")))
-            p = executeAndSubmitAsync(user, service, cmd, not args.no_sudo)
-
-            # run async or join directly
-            if args.async:
-                asyncTasks += [p]
-            else:
-                p.join()
-
-    # wait for all processes to finish if was async
-    for task in asyncTasks:
-        task.join()
-
+            user, service, cmd = list(filter(lambda x: x, line.split("\t")))
+            executeAndSubmit(hostnameIdent, user, service, cmd, args)
 
 if __name__ == '__main__':
 
@@ -104,15 +90,15 @@ if __name__ == '__main__':
                     help='send-nsca configuration file (default set by nsca-package)')
     parser.add_argument('--nsca-bin', default="/usr/sbin/send_nsca",
                     help='send-nsca executable (default: /usr/sbin/send_nsca)')
-    parser.add_argument('-c', '--config', dest='configurationFile', default="monitoring.conf",
+    parser.add_argument('--config', default="monitoring.conf",
                     help='Configuration file (default: ./monitoring.conf)')
-    parser.add_argument('-a', '--async', action="store_const", const=True, default=False,
+    parser.add_argument('--async', action="store_const", const=True, default=False,
                     help='Run checks asynchronous')
-    parser.add_argument('-u', '--ignore-user', action="store_const", const=True, default=False, 
+    parser.add_argument('--ignore-user', action="store_const", const=True, default=False, 
                     help='Run as current user and ignore user column in config file')
 
-    parser.add_argument('-x', '--gateway', help='If set, use an async icinga checks gateway')
-    parser.add_argument('-t', '--gateway-token', help='Token to use with the gateway')
+    parser.add_argument('--gateway', help='If set, use an async icinga checks gateway')
+    parser.add_argument('--gateway-token', help='Token to use with the gateway')
 
     args = parser.parse_args()
     if not args.hostname:
@@ -120,4 +106,4 @@ if __name__ == '__main__':
     else:
         hostname = args.hostname
 
-    executeConfig(hostnameIdent, args)
+    executeConfig(hostname, args)
